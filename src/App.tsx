b84dbd7f75054060b4110272855664c9c0fe6879
boroboro01 <duke7201@naver.com>
2026-01-19 20:33:27 +0900

    feat: add gtm code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import Home from "./pages/Home";
 
 function App() {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: "GTM-NPL3G8VB",
+    });
+  }, []);
+
   return <Home />;
 }
 
